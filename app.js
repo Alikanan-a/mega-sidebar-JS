04d534deef4483791cda0a3c7642bc46326f2c03
alikanan <a.kanan2222@gmail.com>
2023-08-13 23:59:19 +0430

    add rtl and fix jquery

--- app.js
+++ app.js
@@ -1,4 +1,89 @@
-const body  = document.querySelector('body'),
+
+
+    $(function () {
+        let dropdouwnIsOpen = false
+        $('#dashboard').click(function() {
+          if (!dropdouwnIsOpen) {
+            $('#dashboardDropDown').fadeIn()
+            $('#NotificationsDropDown').hide()
+            $('#AnalyticsDropDown').hide()
+            $('#LikesDropDown').hide()
+            $('#WalletsDropDown').hide()
+            $('#RevenueDropdown').hide()
+          } else {
+            $('#dashboardDropDown').hide()
+          }
+          dropdouwnIsOpen = !dropdouwnIsOpen
+        })
+        $('#Revenue').click(function() {
+          if (!dropdouwnIsOpen) {
+            $('#RevenueDropdown').fadeIn()
+            $('#dashboardDropDown').hide()
+            $('#NotificationsDropDown').hide()
+            $('#AnalyticsDropDown').hide()
+            $('#LikesDropDown').hide()
+            $('#WalletsDropDown').hide()
+          } else {
+            $('#RevenueDropdown').hide()
+          }
+          dropdouwnIsOpen = !dropdouwnIsOpen
+        })
+        $('#Notifications').click(function() {
+          if (!dropdouwnIsOpen) {
+            $('#NotificationsDropDown').fadeIn()
+            $('#dashboardDropDown').hide()
+            $('#RevenueDropdown').hide()
+            $('#AnalyticsDropDown').hide()
+            $('#LikesDropDown').hide()
+            $('#WalletsDropDown').hide()
+          } else {
+            $('#NotificationsDropDown').hide()
+          }
+          dropdouwnIsOpen = !dropdouwnIsOpen
+        })
+        $('#Analytics').click(function() {
+          if (!dropdouwnIsOpen) {
+            $('#AnalyticsDropDown').fadeIn()
+            $('#dashboardDropDown').hide()
+            $('#RevenueDropdown').hide()
+            $('#NotificationsDropDown').hide()
+            $('#LikesDropDown').hide()
+            $('#WalletsDropDown').hide()
+          } else {
+            $('#AnalyticsDropDown').hide()
+          }
+          dropdouwnIsOpen = !dropdouwnIsOpen
+        })
+        $('#Likes').click(function() {
+          if (!dropdouwnIsOpen) {
+            $('#LikesDropDown').fadeIn()
+            $('#dashboardDropDown').hide()
+            $('#RevenueDropdown').hide()
+            $('#NotificationsDropDown').hide()
+            $('#AnalyticsDropDown').hide()
+            $('#WalletsDropDown').hide()
+          } else {
+            $('#LikesDropDown').hide()
+          }
+          dropdouwnIsOpen = !dropdouwnIsOpen
+        })
+        $('#Wallets').click(function() {
+          if (!dropdouwnIsOpen) {
+            $('#WalletsDropDown').fadeIn()
+            $('#dashboardDropDown').hide()
+            $('#RevenueDropdown').hide()
+            $('#NotificationsDropDown').hide()
+            $('#AnalyticsDropDown').hide()
+            $('#LikesDropDown').hide()
+          } else {
+            $('#WalletsDropDown').hide()
+          }
+          dropdouwnIsOpen = !dropdouwnIsOpen
+        })
+      })
+
+
+      const body  = document.querySelector('body'),
     sidebar = body.querySelector('.sidebar'),
     toggle  = body.querySelector('.toggle'),
     searchBtn = body.querySelector('.search-box'),
@@ -16,6 +101,6 @@ const body  = document.querySelector('body'),
         if(body.classList.contains('dark')){
             modeText.innerText = 'light Mode'
         }else {
-            modeText.innerText = 'Dark Mode'
+            modeText.innerText = 'Dark Mode'    
         }
     });
